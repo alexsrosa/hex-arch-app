1b9612961956cbd7d599c679491ad02d78ce6332
Alex Rosa <alexsros@gmail.com>
2023-09-21 09:16:50 +0100

    first commit

---
+++ src/main/java/pt/example/hexarchapp/HexArchAppApplication.java
@@ -0,0 +1,13 @@
+package pt.example.hexarchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexArchAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexArchAppApplication.class, args);
+    }
+
+}
